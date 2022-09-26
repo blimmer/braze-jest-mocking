69baed153d7599bc0038fae19ea68b64dbfedc22
Ben Limmer <ben@benlimmer.com>
2022-09-26 13:32:25 -0600

    add failing logic

---
+++ src/braze.js
@@ -0,0 +1,5 @@
+import { initialize } from '@braze/web-sdk'
+
+export function initializeBraze() {
+  initialize('not-a-real-api-key', {})
+}
---
+++ src/braze.test.js
@@ -0,0 +1,7 @@
+import { initializeBraze } from "./braze"
+
+describe('initializeBraze', () => {
+  it('does something', () => {
+    initializeBraze()
+  })
+})
